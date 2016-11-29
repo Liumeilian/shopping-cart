a1b0bc2dd38a4a81351a9bec51e2b10f84c5bb08
hzwangjunjun <hzwangjunjun@corp.netease.com>
2016-11-29 21:16:55 +0800

    继续重构reducer

--- reducers/InitialState.js
+++ reducers/InitialState.js
@@ -87,9 +87,7 @@ export default {
       }
     }
   },
-  cart: {
-    chooseProduct: []
-  }
+  cart: []
 
   /**
    * cart:{
--- reducers/index.js
+++ reducers/index.js
@@ -2,13 +2,7 @@ import { combineReducers } from 'redux';
 import InitialState from './InitialState';
 import * as types from '../constants/actionTypes.js';
 
-function findSameProduct(chooseProductList,needFindProduct){
-  return chooseProductList.find(product=> {return product.productId == needFindProduct.productId});
-}
 
-function getSumPrice(chooseProduct){
-  return chooseProduct.reduce((prev,cur)=>{return prev+=cur.count*cur.price},0)
-}
 
 function updateProductType(productType,product,callback){
    let productId=product.productId,
@@ -60,19 +54,30 @@ function product(state=InitialState.allProduct,action){
   }
 }
 
-function changeChooseProduct(product){
+function addChooseProduct(product,prodect2){
   if(!product){
     return {
-      name:product.name,
-      price:product.price,
+      productId:prodect2.productId,
+      name:prodect2.name,
+      price:prodect2.price,
       count:1
     }
   }else{
     return {
       ...product,
-      count:product+1
+      count:product.count+1
     }
   }
+}
+
+function subChooseProduct(product){
+  if(product.count>0){
+    return{
+      ...product,
+      count:product.count-1
+    }
+  }
+  return product;
 }
 
 function cart(state=InitialState.cart,action){
@@ -80,55 +85,50 @@ function cart(state=InitialState.cart,action){
   
   switch(action.type){
     case types.ADD_PRODUCT:
-    { 
-      let copyList = Object.assign([],state.chooseProduct);
-      let findProduct = findSameProduct(copyList,newProduct)
-      var needAddProduct = 
-      let findIndex = copyList.findIndex(product=>{return product.name==newProduct.name});
-      findIndex>=0?copyList.splice(findIndex,1,needAddProduct):copyList.push(needAddProduct);
-      
-      return Object.assign({},state,{
-        chooseProduct:copyList
-      })
-      return [
-        ...state,
-        changeChooseProduct(findSameProduct(state,action.product))
-      ]
-    }
-    case types.SUB_PRODUCT:
-    { 
-      
-      let copyList = Object.assign([],state.chooseProduct);
-      let findProduct = findSameProduct(copyList,newProduct)
-      if(findProduct.count>1){
-        findProduct--;
+      if(state.length==0){
+        return [...state,{
+                productId:action.product.productId,
+                name:action.product.name,
+                price:action.product.price,
+                count:1
+              }]
       }else{
-        let findIndex = copyList.findIndex(product=>{return product.name==newProduct.name});
-        copyList.splice(findIndex,1)
+        return state.map(obj => {
+          if(obj.productId == action.product.productId){
+            return {...obj,count:obj.count+1}
+          }else{
+            return obj
+          }
+        })
       }
-      return Object.assign({},state,{
-        sumPrice:getSumPrice(copyList),
-        chooseProduct:copyList
-      })
-    }
+    case types.SUB_PRODUCT:
+      return state.map(obj => {
+          if(obj.productId == action.product.productId){
+            return {...obj,count:obj.count-1}
+          }else{
+            return obj
+          }
+        })
     default:
       return state
   }
 }
-
-
 
 const rootReducer = combineReducers({
   product,
   cart
 })
 
+function getSumPrice(chooseProduct){
+  return chooseProduct.reduce((prev,cur)=>{return prev+=cur.count*cur.price},0)
+}
+
 export default rootReducer;
 
 export function getCartState(state){
   return {
-    price:getSumPrice(state.cart.chooseProduct),
-    count:state.cart.chooseProduct.reduce((prev,current)=>{return prev+current.count},0)
+    price:getSumPrice(state.cart),
+    count:state.cart.reduce((prev,current)=>{return prev+current.count},0)
   }
 }
 
